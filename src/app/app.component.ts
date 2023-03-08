82aed371f8a6bceb0147d8786921b201a8c516d8
MaxChe <Max@HOME-PC>
2023-03-08 19:12:04 +0300

    Начал подготовку.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { map } from 'rxjs/operators'
-import {User} from './user';
+import { ToDoItem } from './ToDoItem';
 
  
 @Component({
     selector: 'my-app',
-    template: `<p>ID: {{user?.id}}</p>
-    <p>Age: {{user?.age}}</p>
-    <p>FirstName: {{user?.firstName}}</p>`
+    templateUrl: './app.component.html'
 })
 
 
 export class AppComponent implements OnInit {
     constructor(private http: HttpClient){}
 
-    user: User | undefined 
-
+    
     ngOnInit(){
-       this.http.get('https://localhost:7218/User/GetUser/2').subscribe({next: (data: any) => this.user = new User(data.id, data.age, data.firstName)});
+       
     }     
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,7 @@
+import { ToDoItem } from "./ToDoItem";
+
 export class DataService{
     getUserById(id: number){
 
     }
 }
-
-
-export class User{
-    constructor(public id:number, public age: number, public firstName: string, public middleName: string, public lastName: string,
-        public hobby: string, public city: string){
-    }
-}
-
-let Users: User[];
