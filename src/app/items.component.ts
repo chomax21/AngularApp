2452edfbbf52a6367304693a315e084a396c4a77
chomax21 <maximcheb21@gmail.com>
2023-03-12 19:56:24 +0300

    Создал метод для получения списка дел с сервера.

---
+++ src/app/items.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { ToDoService } from './ToDoService'
+import { ToDoItem } from './ToDoItem';
+
+
+@Component({
+    selector: 'items-comp',
+    templateUrl: '',
+    styleUrls: [``]
+})
+export class ItemComponents{
+    items[]:ToDoItem = 
+}
