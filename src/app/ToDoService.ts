ac8c9420ecad981a429780214cb04d7a4a95c6af
chomax21 <maximcheb21@gmail.com>
2023-03-13 16:00:35 +0300

    Созданы 2 метода: GetUserId & getDoLists

--- src/app/ToDoService.ts
+++ src/app/ToDoService.ts
@@ -1,16 +1,29 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { ToDoItem } from './ToDoItem';
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 
 
 @Injectable()
 export class ToDoService{
     constructor(private http: HttpClient) { }
 
-    httpHeaders = new HttpHeaders().set('Media type', 'text/plain');
-
     createToDoItem(item:ToDoItem){
         return this.http.post("https://localhost:7218/api/create", item);
+    }
+
+    getUserId(login:string, password:string){
+        return this.http.get('https://localhost:7218/api/get-user-id?login=' + login + ',password=' + password);
+    }
+
+    getDoLists(Id:string): Observable<ToDoItem[]>{
+        return this.http.get('https://localhost:7218/api/getnotready?Id=' + Id).pipe(map((data:any)=>{
+            let itemList = data["Values"];
+            return itemList.map(function(item:any):ToDoItem{
+                return new ToDoItem(item.Id, item.Priority, item.Case);
+            })
+        } ))
     }
 
 }
